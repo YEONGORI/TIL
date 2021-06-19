9963093dbf9970a739a27689ea4b8a8528e15074
gori <dusrjf14@gmail.com>
2021-06-19 15:35:07 +0900

    .

---
+++ instar/components/Main.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react"
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs"
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { fetchUser } from "../redux/actions/index"
+
+import FeedScreen from "./main/Feed"
+import AddScreen from "./main/Add"
+import ProfileScreen from "./main/Profile"
+
+const Tab = createMaterialBottomTabNavigator()
+
+const EmptyScreen = () => {
+  return null
+}
+
+export class Main extends Component {
+  componentDidMount() {
+    this.props = fetchUser()
+  }
+  render() {
+    return (
+      <Tab.Navigator initialRouteName="Feed" labeled={false}>
+        <Tab.Screen
+          name="Feed"
+          component={FeedScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="AddContaoner"
+          component={EmptyScreen}
+          listeners={({ navigation }) => ({
+            tabPress: (event) => {
+              event.preventDefault()
+              navigation.navigate("Add")
+            },
+          })}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-circle"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    )
+  }
+}
+
+const mapStateProps = (store) => ({
+  currentUser: store.userState.currentUser,
+})
+
+const mapDispatchProps = (dispatch) => {
+  bindActionCreators({ fetchUvser }, dispatch)
+}
+
+export default connect(mapStateProps, mapDispatchProps)(Main)
