ce23f1d42c6ca8d731c24a526615bc73947c048e
yeongori <dusrjf14@gmail.com>
2023-07-05 09:06:43 +0900

    [feat] spring server test

---
+++ back-end/spring/ED/se_server/src/test/java/com/example/se_server/SeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
