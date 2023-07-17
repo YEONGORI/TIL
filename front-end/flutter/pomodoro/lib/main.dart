4c2708fd5f2683f01e328721c648bf0673834d58
yeongori <dusrjf14@gmail.com>
2023-07-17 17:29:35 +0900

    [feat] flutter를 사용해 웹툰 앱 만들기 시작

---
+++ front-end/flutter/pomodoro/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_srceen.dart';
+import 'package:pomodoro/services/api_service.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
