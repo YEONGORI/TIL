a73f9740e3697219ddc9bbf810341696dca711bd
yeongori <dusrjf14@gmail.com>
2023-07-25 16:52:12 +0900

    [feat] 스프링 핵심 원리 기본편 끝

--- back-end/spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ back-end/spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,21 +3,19 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // 생성자 주입의 롬복 사용버전
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
     /*
     생성자 주입
     private final MemberRepository memberRepository;
